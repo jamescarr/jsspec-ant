25518682fc90dc158e21c61e4050c3763e247dba
jamescarr <jamescarr@abidos.(none)>
2009-05-27 20:59:54 -0500

    added result extractor and example can get names

--- src/main/java/org/jamescarr/jsspec/runner/webdriver/SpecRunner.java
+++ src/main/java/org/jamescarr/jsspec/runner/webdriver/SpecRunner.java
@@ -6,27 +6,37 @@ import org.openqa.selenium.WebDriver;
 public class SpecRunner {
 
 	private final WebDriver driver;
+	private boolean running = false;
+	private ResultExtractor<JSSpecResult> extractor;
 
 	public SpecRunner(WebDriver driver) {
 		this.driver = driver;
 	}
 
 	public JSSpecResult execute(String specName) {
+		running = true;
 		driver.navigate().to(specName);
 		while(!driver.findElement(By.id("progress")).getText().equals("100")){
 			try {
-				Thread.sleep(500);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				
 			}
 		}
+		running = false;
 		return createResult();
 	}
 
 	private JSSpecResult createResult() {
-		JSSpecResult specResult = new JSSpecResult();
-		specResult.setExamples(Integer.parseInt(driver.findElement(By.id("total_examples")).getText()));
-		return specResult;
+		return extractor.generate(driver);
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setMetricExtractor(ResultExtractor<JSSpecResult> extractor) {
+		this.extractor = extractor;
+		
 	}
 }
--- src/test/java/org/jamescarr/jsspec/runner/webdriver/SpecRunnerTest.java
+++ src/test/java/org/jamescarr/jsspec/runner/webdriver/SpecRunnerTest.java
@@ -1,24 +1,24 @@
 package org.jamescarr.jsspec.runner.webdriver;
 
-import static org.junit.Assert.assertEquals;
+import static org.jamescarr.jsspec.runner.helpers.JsSpecHelpers.using;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.File;
-import java.net.MalformedURLException;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Navigation;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import static org.jamescarr.jsspec.runner.helpers.JsSpecHelpers.localSpec;
 
 public class SpecRunnerTest {
 	private WebDriver driver;
 	private SpecRunner runner;
-	
+	private ResultExtractor extractor;
+	private Thread t ;
 	@Before
 	public void before(){
 		driver = mock(WebDriver.class);
@@ -26,12 +26,53 @@ public class SpecRunnerTest {
 		when(driver.navigate()).thenReturn(nav);
 		
 		runner = new SpecRunner(driver);
+		using(driver).id("progress").returnsText("100");
+		
+		extractor = mock(ResultExtractor.class);
+		runner.setMetricExtractor(extractor);
+	}
+	@After
+	public void after(){
+		if(t != null && t.isAlive()){t.interrupt();}
+	}
+	public void run(Runnable runner){
+			t = new Thread(runner);
+			t.start();
+	}
+	@Test
+	public void shouldCompleteWhenProgressHasReachedOneHundred() throws InterruptedException{
+		using(driver).id("progress").returnsText("50");
+		
+		runner = new SpecRunner(driver);
+		run(new Runnable(){
+			public void run() {
+				runner.execute("some-spec.html");
+			}
+		});
+		
+		Thread.sleep(500);
+		assertTrue(runner.isRunning());
+		
+		using(driver).id("progress").returnsText("100");
+		Thread.sleep(500);
+		
+		assertFalse(runner.isRunning());
 	}
 	@Test
 	public void shouldNavigateToSpecifiedUrl(){	
 		runner.execute("file:///C|/foo/somefile.html");
 		
 		verify(driver.navigate()).to("file:///C|/foo/somefile.html");
+	}
+	
+	@Test
+	public void shouldUseMetricExtractorToExtractMetrics(){
+		JSSpecResult expectedResult = new JSSpecResult();
+		when(extractor.generate(driver)).thenReturn(expectedResult);
+		
+		JSSpecResult result = runner.execute("file:///C|/foo/somefile.html");
+	
+		assertSame(result, expectedResult);
 	}
 	
 }
